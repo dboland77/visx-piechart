150320eecc1b4d1a8e1f2863baabd6b6c73b85aa
Dave Boland <dboland77@gmail.com>
2022-02-09 11:13:32 +0000

    Add pie segmentation and colouring

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,13 @@ import { Text } from "@visx/text";
 const coins = [
   { symbol: "ADA", amount: 200, colour: "#0033ad", inGBP: 1.48 },
   { symbol: "SOL", amount: 5, colour: "#00ffbd", inGBP: 37.6 },
-  { symbol: "BTC", amount: 0.005, colour: "#0033ad", inGBP: 37363 },
+  { symbol: "BTC", amount: 0.005, colour: "#F7931A", inGBP: 37363 },
 ];
 
 export default function Home() {
   const width = 400;
   const half = width / 2;
+  const padSize = 8;
 
   return (
     <main>
@@ -21,7 +22,20 @@ export default function Home() {
           data={coins} 
           pieValue={data => data.amount*data.inGBP}
           outerRadius={half}
-          ></Pie>
+          innerRadius={half-padSize}
+          padAngle = {0.01}
+          >
+            {pie => {
+              return pie.arcs.map(arc => {
+                return (
+                  <g key ={arc.data.symbol}>
+                    <path d={pie.path(arc)} fill={arc.data.colour}></path>
+                  </g>)
+              })
+            }}
+
+
+          </Pie>
         </Group>
       </svg>
     </main>
