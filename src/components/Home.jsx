0bb828e1934b0702a4e1618f0356351cb79e0b3a
Dave Boland <dboland77@gmail.com>
2022-02-09 10:57:05 +0000

    Adding visx packages and initial setup

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Pie } from "@visx/shape";
+import { Group } from "@visx/group";
+import { Text } from "@visx/text";
+
+const coins = [
+  { symbol: "ADA", amount: 200, colour: "#0033ad", inGBP: 1.48 },
+  { symbol: "SOL", amount: 5, colour: "#00ffbd", inGBP: 37.6 },
+  { symbol: "BTC", amount: 0.005, colour: "#0033ad", inGBP: 37363 },
+];
+
+export default function Home() {
+  return <main>PIE</main>
+}
